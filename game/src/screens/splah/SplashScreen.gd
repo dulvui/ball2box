c8e3fb0ba488f44f808595d36c29356c4ef2ac9b
dulvui <dulvui@posteo.de>
2022-09-19 21:55:57 +0200

    fix splashscreen bug showing weird 3d scene on start

--- game/src/screens/splah/SplashScreen.gd
+++ game/src/screens/splah/SplashScreen.gd
@@ -1,8 +1,4 @@
 extends Control
 
 func _ready():
-	$AnimationPlayer.play("Go")
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	get_tree().paused = true
 	get_tree().change_scene("res://src/levels/Level%s.tscn"%str(Global.current_level))
