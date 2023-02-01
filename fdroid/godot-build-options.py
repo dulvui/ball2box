d5a3ea11b997ef6f3df90e6f0370f4edb210c6bd
dulvui <github@simondalvai.com>
2023-02-01 18:28:11 +0100

    create fdroid folder

---
+++ fdroid/godot-build-options.py
@@ -0,0 +1,16 @@
+# Generated using https://godot-build-options-generator.github.io
+
+disable_advanced_gui = "yes"
+module_bullet_enabled = "no"
+module_csg_enabled = "no"
+module_dds_enabled = "no"
+module_enet_enabled = "no"
+module_mobile_vr_enabled = "no"
+module_multiplayer_enabled = "no"
+module_noise_enabled = "no"
+module_openxr_enabled = "no"
+module_upnp_enabled = "no"
+module_visual_script_enabled = "no"
+module_webrtc_enabled = "no"
+module_websocket_enabled = "no"
+module_webxr_enabled = "no"
