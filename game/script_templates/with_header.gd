6f3a737fbd25e94c3c2eceefa0993097f9dca8be
Simon Dalvai <git@s9i.cc>
2023-07-20 21:30:22 +0200

    godot: create script template with license header

---
+++ game/script_templates/with_header.gd
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.com>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+extends %BASE%
+
+func _ready()%VOID_RETURN%:
+    pass
+
+func _process(delta%FLOAT_TYPE%)%VOID_RETURN%:
+   pass
