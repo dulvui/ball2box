314ba17d6512d54583165be516c7958fec73f63e
Simon Dalvai <info@simondalvai.org>
2024-02-01 08:55:42 +0100

    base-object: use vars for nodes

--- game/src/actors/objects/base-object/BaseObject.gd
+++ game/src/actors/objects/base-object/BaseObject.gd
@@ -9,10 +9,17 @@ onready var animation_player = $AnimationPlayer
 
 export var delay:float = 0
 
+var static_body:StaticBody
+var area:Area
+
 var hit:bool = false
 var new:bool = true
 
 func _ready() -> void:
+	if has_node("Body/Area"):
+		area = $Body/Area
+	if has_node("Body/MeshInstance/StaticBody"):
+		static_body = $Body/MeshInstance/StaticBody
 	hide()
 	
 func fade_in() -> void:
@@ -28,10 +35,10 @@ func fade_in() -> void:
 			animation_player.play("Act")
 			
 		if has_node("Body/Area"):
-			$Body/MeshInstance/StaticBody.set_collision_layer_bit(0,true)
-			$Body/MeshInstance/StaticBody.set_collision_mask_bit(0,true)
-			$Body/Area.set_collision_layer_bit(1,true)
-			$Body/Area.set_collision_mask_bit(1,true)
+			static_body.set_collision_layer_bit(0,true)
+			static_body.set_collision_mask_bit(0,true)
+			area.set_collision_layer_bit(1,true)
+			area.set_collision_mask_bit(1,true)
 	
 	if hit and animation_player.is_playing():
 			hit = false
@@ -45,19 +52,19 @@ func fade_in() -> void:
 			if animation_player.has_animation("Act"):
 				animation_player.play("Act")
 				
-			if has_node("Body/Area"):
-				$Body/MeshInstance/StaticBody.set_collision_layer_bit(0,true)
-				$Body/MeshInstance/StaticBody.set_collision_mask_bit(0,true)
-				$Body/Area.set_collision_layer_bit(1,true)
-				$Body/Area.set_collision_mask_bit(1,true)
+			if area:
+				static_body.set_collision_layer_bit(0,true)
+				static_body.set_collision_mask_bit(0,true)
+				area.set_collision_layer_bit(1,true)
+				area.set_collision_mask_bit(1,true)
 
 
 func _on_Area_body_entered(body) -> void:
 	if body.is_in_group("ball"):
-		$Body/MeshInstance/StaticBody.set_collision_layer_bit(0,false)
-		$Body/MeshInstance/StaticBody.set_collision_mask_bit(0,false)
-		$Body/Area.set_collision_layer_bit(1,false)
-		$Body/Area.set_collision_mask_bit(1,false)
+		static_body.set_collision_layer_bit(0,false)
+		static_body.set_collision_mask_bit(0,false)
+		area.set_collision_layer_bit(1,false)
+		area.set_collision_mask_bit(1,false)
 		hit = true
 		animation_player.play("Pop")
 		yield(animation_player,"animation_finished")
