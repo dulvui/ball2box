a0d1ae99586788f3dda39e3aed5f264c8c6b962d
Simon Dalvai <info@simondalvai.org>
2024-07-14 11:05:47 +0200

    level editor: create objects util and load them

--- game/src/level-editor/LevelEditor.gd
+++ game/src/level-editor/LevelEditor.gd
@@ -5,15 +5,18 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_Add_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Undo_pressed():
+	pass # Replace with function body.
+
+
+func _on_Redo_pressed():
+	pass # Replace with function body.
+
+
+func _on_Delete_pressed():
+	pass # Replace with function body.
---
+++ game/src/level-editor/ObjectsUtil.gd
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+extends Node
+
+
+const OBJECTS_PATH: String = "res://src/actors/objects/"
+
+var objects: Array = []
+
+func _ready() -> void:
+	var dir: Directory = Directory.new()
+	if dir.open(OBJECTS_PATH) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				print("Found file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access objects path.")
+
