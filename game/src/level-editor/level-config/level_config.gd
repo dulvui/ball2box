ec27c242028d2c8cb104b1412e352e3a450b1c9c
Simon Dalvai <info@simondalvai.org>
2024-07-26 18:30:29 +0200

    level-editor: create config classes

---
+++ game/src/level-editor/level-config/level_config.gd
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class_name LevelConfig
+
+var version: String
+var objects: Array
+var star1: ObjectConfig
+var star2: ObjectConfig
+var ball: ObjectConfig
+var box: ObjectConfig
---
+++ game/src/level-editor/level-config/object_config.gd
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.org>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+class_name ObjectConfig
+
+var id: String
+var x: float
+var y: float
+var z: float
+var rotation: float
+var scale: float
+var animation_delay: float
